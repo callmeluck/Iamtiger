bddc95345b569a02ec641244582687038a953eae
callmeluck <18832409858@163.com>
2017-07-12 02:51:25 +0800

    Add files via upload

---
+++ js/animate.js
@@ -0,0 +1,22 @@
+$('.page_1 h1').addClass('animated bounceInDown');
+$('.page_2 p').addClass('animated fadeInLeftBig');
+$('.pagr_3_left').addClass('animated rotateInDownLeft');
+$('.pagr_3_right').addClass('animated rotateInDownRight');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
